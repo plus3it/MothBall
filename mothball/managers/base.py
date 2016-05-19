7da9b1dc6d13d7f80c0b529c9a780fc43f5dd74e
marion <the.marion.cobretti@gmail.com>
2016-05-19 13:55:56 -0400

    added pulling all the info for EBS and EIP per instance for all instances.

--- mothball/managers/base.py
+++ mothball/managers/base.py
@@ -1,14 +1,18 @@
+import json
 import boto3
 import logging
 
 class AWSManager(object):
 
     def __init__(self, region, key, secret):
+        # type: str, str, str
+
         self.region = region
         self.key = key
         self.secret = secret
         self.ec2_instances = None
         self.ec2_session = None
+        self.data = dict()
 
         self.Session = boto3.Session(
             region_name=self.region,
@@ -28,6 +32,7 @@ class AWSManager(object):
 
     def _validate_ec2_region(self):
 
+
         ec2 = boto3.client('ec2')
         regions = ec2.describe_regions()
 
@@ -43,12 +48,63 @@ class AWSManager(object):
 
         self.ec2_instances = instlist
 
-    def _get_ebs_volume_info(self):
-        pass
+    def _get_ebs_volume_info(self, instance):
 
-    def _get_eip_info(self):
-        pass
+        device_structure = self.ec2_session.Instance(instance).block_device_mappings
 
+        if 'EBS' in self.data[instance]:
+            logging.warning('EBS Data already exists in instance {0}'.format(instance))
+        else:
+            self.data[instance]['EBS'] = dict()
 
+        for dev in device_structure:
+            device = dict()
+            devvolid = dev['Ebs']['VolumeId']
+            device['Mount'] = dev['DeviceName']
+            device['Size'] = self.ec2_session.Volume(devvolid).size
+            device['Type'] = self.ec2_session.Volume(devvolid).volume_type
+            device['IOPS'] = self.ec2_session.Volume(devvolid).iops
+            device['AZ'] = self.ec2_session.Volume(devvolid).availability_zone
+            device['Tags'] = json.dumps(self.ec2_session.Volume(devvolid).tags)
 
+            self.data[instance]['EBS'][devvolid] = device
+
+    def _get_eip_info(self, instance):
+
+        instIfaces = self.ec2_session.Instance(instance).network_interfaces_attribute
+
+        if 'EIP' in self.data[instance]:
+            logging.warning('EIP Data already exists in instance {0}'.format(instance))
+        else:
+            self.data[instance]['EIP'] = dict()
+
+        for interface in instIfaces:
+            eipNetIfId = interface['NetworkInterfaceId']
+            self.data[instance]['EIP'][eipNetIfId] = dict()
+            self.data[instance]['EIP'][eipNetIfId]['description'] = interface['Description']
+            self.data[instance]['EIP'][eipNetIfId]['macAddress'] = interface['MacAddress']
+            self.data[instance]['EIP'][eipNetIfId]['interfaceId'] = interface['NetworkInterfaceId']
+            self.data[instance]['EIP'][eipNetIfId]['accountId'] = interface['OwnerId']
+            self.data[instance]['EIP'][eipNetIfId]['privateDns'] = interface['PrivateDnsName']
+            self.data[instance]['EIP'][eipNetIfId]['privateIpAddr'] = interface['PrivateIpAddress']
+            self.data[instance]['EIP'][eipNetIfId]['SrcDstChk'] = interface['SourceDestCheck']
+            self.data[instance]['EIP'][eipNetIfId]['Status'] = interface['Status']
+            self.data[instance]['EIP'][eipNetIfId]['SubnetId'] = interface['SubnetId']
+            self.data[instance]['EIP'][eipNetIfId]['VpcId'] = interface['VpcId']
+            if 'Association' in interface:
+                self.data[instance]['EIP'][eipNetIfId]['publicDns'] = interface['Association'].get('PublicDnsName')
+                self.data[instance]['EIP'][eipNetIfId]['publicIpAddr'] = interface['Association'].get('PublicIp')
+
+    def get_info(self):
+
+        if not self.ec2_instances:
+            self._get_ec2_instances()
+
+        for instance in self.ec2_instances:
+            if instance in self.data:
+                logging.warning('Instance {0} already exists in Data?'.format(instance))
+
+            self.data[instance] = dict()
+            self._get_ebs_volume_info(instance)
+            self._get_eip_info(instance)
 
