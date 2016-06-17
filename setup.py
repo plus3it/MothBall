cd2d673d8b1eba0d2cd17d2f4cc94bd71fbc274a
marion <the.marion.cobretti@gmail.com>
2016-06-17 15:20:40 -0400

    Updated to install static files

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = '0.1',
     packages = find_packages(),
     include_package_data = True,
+    data_files=['static/mothball.config'],
     install_requires = [ 'sqlalchemy',
                          'boto3',
                          'pyyaml'
