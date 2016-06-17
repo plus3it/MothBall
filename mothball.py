a92c9527c6f76ec6a5a855e4b7f9ec894e4f41b1
marion <the.marion.cobretti@gmail.com>
2016-06-17 15:14:10 -0400

    Added default config. Created MANIFEST.in to include config into packages. Removed old ini support.

---
+++ mothball.py
@@ -0,0 +1,51 @@
+import os
+import yaml
+import logging
+import argparse
+
+from mothball.managers.base import AWSManager
+
+def get_config(filename):
+
+    config = None
+
+    if os.path.exists(filename):
+        with open(filename) as f:
+            data = f.read()
+
+        config = yaml.load(data)
+    else:
+        logging.error('config file does not exist!')
+
+    return config
+
+def main(filename):
+
+    config = get_config(filename)
+    aws = AWSManager(config['AWS']['region'],
+                     config['AWS']['access_key'],
+                     config['AWS']['secret_key'],
+                     config['Database']['username'],
+                     config['Database']['password'],
+                     config['Database']['name'],
+                     config['Database']['host'],
+                     config['Database']['port'],
+                     config['Database']['type'],
+                     config['RDS']['use_rds'],
+                     config['RDS']['name'],
+                     config['RDS']['vpc_sgs'])
+
+    aws.get_account_info()
+    aws.get_db_connection()
+    aws.get_info()
+
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', dest='filename', default='mothball.config')
+
+
+    main(parser.parse_args().filename)
